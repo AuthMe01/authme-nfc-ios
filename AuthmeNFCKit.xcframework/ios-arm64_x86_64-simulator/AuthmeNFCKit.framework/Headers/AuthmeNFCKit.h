9be0f78e34fad953f48c1f15cb6fa2e6ded0e884
AuthMe-Team <dev@authme.com>
2024-04-19 07:32:19 +0000

    Release AuthmeNFCKit 0.9.0

---
+++ AuthmeNFCKit.xcframework/ios-arm64_x86_64-simulator/AuthmeNFCKit.framework/Headers/AuthmeNFCKit.h
@@ -0,0 +1,18 @@
+//
+//  AuthmeNFCKit.h
+//  AuthmeNFCKit
+//
+//  Created by TimChen on 2023/12/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AuthmeNFCKit.
+FOUNDATION_EXPORT double AuthmeNFCKitVersionNumber;
+
+//! Project version string for AuthmeNFCKit.
+FOUNDATION_EXPORT const unsigned char AuthmeNFCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthmeNFCKit/PublicHeader.h>
+
+
